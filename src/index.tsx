756631431ddac91b1fbb5584c45de8ca5054a775
Jascha-Biederstedt <j.biederstedt@gmx.de>
2021-08-04 12:38:54 +0200

    Initialize esbuild

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,38 @@
+import * as esbuild from 'esbuild-wasm';
 import ReactDOM from 'react-dom';
+import { useState, useEffect } from 'react';
 
 const App = () => {
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+
+  const startService = async () => {
+    const service = await esbuild.startService({
+      worker: true,
+      wasmURL: './esbuild.wasm',
+    });
+
+    console.log(service);
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const onClick = () => {
+    console.log(input);
+  };
+
   return (
     <div>
-      <textarea></textarea>
+      <textarea
+        value={input}
+        onChange={e => setInput(e.target.value)}
+      ></textarea>
       <div>
-        <button>Submit</button>
+        <button onClick={onClick}>Submit</button>
       </div>
-      <pre></pre>
+      <pre>{code}</pre>
     </div>
   );
 };
