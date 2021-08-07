79b54f3128856007b274b6a3ed9036a5e2c830c7
Jascha-Biederstedt <j.biederstedt@gmx.de>
2021-08-07 12:40:52 +0200

    Fix syntax highlighting

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -2,8 +2,11 @@ import { useRef } from 'react';
 import MonacoEditor, { EditorDidMount } from '@monaco-editor/react';
 import prettier from 'prettier';
 import parser from 'prettier/parser-babel';
+import codeshift from 'jscodeshift';
+import Highlighter from 'monaco-jsx-highlighter';
 
 import './code-editor.css';
+import './syntax.css';
 
 interface CodeEditorProps {
   initialValue: string;
@@ -21,6 +24,20 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }) => {
     });
 
     monacoEditor.getModel()?.updateOptions({ tabSize: 2 });
+
+    const highlighter = new Highlighter(
+      // @ts-ignore
+      window.monaco,
+      codeshift,
+      monacoEditor
+    );
+
+    highlighter.highLightOnDidChangeModelContent(
+      () => {},
+      () => {},
+      undefined,
+      () => {}
+    );
   };
 
   const onFormatClick = () => {
