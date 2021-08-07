d9fd6eba5eac55b2aab539fdd11e3f9da9f492bd
Jascha-Biederstedt <j.biederstedt@gmx.de>
2021-08-07 10:27:23 +0200

    Configure the editor

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,7 +1,23 @@
 import MonacoEditor from '@monaco-editor/react';
 
 const CodeEditor = () => {
-  return <MonacoEditor height="300px" />;
+  return (
+    <MonacoEditor
+      theme="dark"
+      language="javascript"
+      height="300px"
+      options={{
+        wordWrap: 'on',
+        minimap: { enabled: false },
+        showUnused: false,
+        folding: false,
+        lineNumbersMinChars: 3,
+        fontSize: 16,
+        scrollBeyondLastLine: false,
+        automaticLayout: true,
+      }}
+    />
+  );
 };
 
 export default CodeEditor;
