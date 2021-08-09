404593c1436dff0f3d3a69448c647dd2edd31de8
Jascha-Biederstedt <j.biederstedt@gmx.de>
2021-08-09 13:52:59 +0200

    Update cell logic

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -26,7 +26,18 @@ const reducer = (
     case ActionType.MOVE_CELL:
       return state;
     case ActionType.UPDATE_CELL:
-      return state;
+      const { id, content } = action.payload;
+
+      return {
+        ...state,
+        data: {
+          ...state.data,
+          [id]: {
+            ...state.data[id],
+            content,
+          },
+        },
+      };
     case ActionType.DELETE_CELL:
       return state;
     case ActionType.INSERT_CELL_BEFORE:
