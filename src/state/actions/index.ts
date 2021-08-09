cd032272f09ebf0f24f6359b1afc1197a923cb0b
Jascha-Biederstedt <j.biederstedt@gmx.de>
2021-08-09 13:41:40 +0200

    Add action creators

--- src/state/actions/index.ts
+++ src/state/actions/index.ts
@@ -1,15 +1,17 @@
 import { ActionType } from '../action-types';
 import { CellTypes } from '../cell';
 
-interface MoveCellAction {
+export type Direction = 'up' | 'down';
+
+export interface MoveCellAction {
   type: ActionType.MOVE_CELL;
   payload: {
     id: string;
-    direction: 'up' | 'down';
+    direction: Direction;
   };
 }
 
-interface UpdateCellAction {
+export interface UpdateCellAction {
   type: ActionType.UPDATE_CELL;
   payload: {
     id: string;
@@ -17,12 +19,12 @@ interface UpdateCellAction {
   };
 }
 
-interface DeleteCellAction {
+export interface DeleteCellAction {
   type: ActionType.DELETE_CELL;
   payload: string;
 }
 
-interface InsertCellBeforeAction {
+export interface InsertCellBeforeAction {
   type: ActionType.INSERT_CELL_BEFORE;
   payload: {
     id: string;
